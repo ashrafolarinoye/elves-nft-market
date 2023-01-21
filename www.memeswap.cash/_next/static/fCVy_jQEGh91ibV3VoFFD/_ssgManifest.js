e70c3ec5325b172d9c7a82bbaaf20a93efe6befa
ashrafolarinoye <ashrafolarinoye@gmail.com>
2023-01-21 11:47:33 +0100

    first commit

---
+++ www.memeswap.cash/_next/static/fCVy_jQEGh91ibV3VoFFD/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcollection\u002F[contract]\u002F[tokenID]","\u002Fcollection\u002F[contract]","\u002Fwallet\u002F[user]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
